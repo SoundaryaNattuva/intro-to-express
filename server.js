a2ff5cfcfdf599a4e2a0e0b98f8edc1dcaecb9a1
SoundaryaNattuva <soundaryanatts@gmail.com>
2024-01-10 09:20:16 -0500

    change line 20 to play with syntax

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.get('/', function (req, res){
 
 app.get('/attendance166', function(req, res){
     res.render('attendance/waffles/index',{
-        students: students
+        persons: students
     })
 })
 
